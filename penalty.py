0b4e6cd4f85af925115c2b5f5d58b5198d5bc786
edsinkevic <sinkevicedwin@gmail.com>
2022-12-15 08:53:47 +0200

    Split things

--- penalty.py
+++ penalty.py
@@ -1,9 +1,10 @@
 import numpy as np
 import numpy.linalg as linalg
 from calculations import b, g, f, h
+from minimize import minimize
 from nelder_mead import nelder_mead
-points = [[0., 0., 0.], [1., 1., 1.], [0, 3 / 10, 7 / 10]]
 
+points = [[0., 0., 0.], [1., 1., 1.], [0, 3 / 10, 7 / 10]]
 
 def solve_0():
     for point in points:
@@ -43,42 +44,8 @@ def try_starting(point, reduction, rs):
     return result
 
 def solve_one(point, r, reduction):
-    global f_count
-    global min_count
-    f_count = 0
-    min_count = 0
-    result = minimize(point, r, reduction, [])
+    [result, f_count, min_count] = minimize(point, r, reduction, [])
     return [point, result, f_count, min_count]
-
-min_count = 0
-
-def minimize(point, r, reduction, last_result, epsilon = 1e-6):
-    global min_count
-    br = inject_r(r)
-    min_count += 1
-    result = nelder_mead(br, np.array(point), epsilon = epsilon)
-
-    if not_converging(f, last_result, result):
-        return minimize(result, r - r * reduction, reduction, result)
-    return result
-
-
-f_count = 0
-
-def inject_r(r):
-    def func(X):
-        global f_count
-        f_count += 1
-        return b(X, r)
-
-    return func
-
-def not_converging(f, last_result, result, epsilon = 1e-6):
-    if len(last_result) == 0:
-        return True
-    if abs(f(last_result) - f(result)) > epsilon:
-        return True
-    return False
 
 solve_0()
 solve_1()
